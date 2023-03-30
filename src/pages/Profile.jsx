1cacf2f0e073337e5be58d2b0c372fa35a81e277
Costi Cretu <costi.cretu25@gmail.com>
2023-03-30 18:02:58 +0300

    create listing page UI done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import { getAuth, updateProfile } from 'firebase/auth'
 import { useNavigate } from 'react-router'
 import { toast } from 'react-toastify'
-import {updateDoc, doc } from 'firebase/firestore'
-import {db} from '../firebase'
+import { updateDoc, doc } from 'firebase/firestore'
+import { db } from '../firebase'
+import { FcHome } from 'react-icons/fc'
+import { Link } from 'react-router-dom'
 
 export default function Profile() {
   const auth = getAuth()
@@ -26,7 +28,7 @@ export default function Profile() {
   }
   async function onSubmit() {
     try {
-      if(auth.currentUser.displayName !== name) {
+      if (auth.currentUser.displayName !== name) {
         //update the displayName in firebase auth
         await updateProfile(auth.currentUser, {
           displayName: name,
@@ -35,7 +37,7 @@ export default function Profile() {
         const docRef = doc(db, 'users', auth.currentUser.uid)
         await updateDoc(docRef, {
           name: name,
-        }) 
+        })
       }
       toast.success('Profile details updated')
     } catch (error) {
@@ -48,11 +50,11 @@ export default function Profile() {
         <h1 className='text-3xl text-center mt-6 font-bold'>My Profile</h1>
         <div className='w-full md:w-[50%] mt-6 px-3'>
           <form>
-            <input type="text" id='name' value={name} disabled={!changeDetail} onChange={onChange} 
-            className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out 
+            <input type="text" id='name' value={name} disabled={!changeDetail} onChange={onChange}
+              className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out 
             ${changeDetail && "bg-red-200 focus:bg-red-200"}`} />
-            <input type="email" id='email' value={email} disabled 
-            className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out' />
+            <input type="email" id='email' value={email} disabled
+              className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out' />
             <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
               <p className='flex items-center mb-6'>Do you want to change your name?
                 <span onClick={() => {
@@ -64,6 +66,12 @@ export default function Profile() {
               <p onClick={onLogout} className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'>Sign out</p>
             </div>
           </form>
+          <button type="submit" className='w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'>
+            <Link to='/create-listing' className='flex justify-center items-center'>
+              <FcHome className='mr-2 text-3xl bg-red-200 rounded-full p-1 border-2'/>
+              Sell or rent your home
+            </Link>
+          </button>
         </div>
       </section>
     </>

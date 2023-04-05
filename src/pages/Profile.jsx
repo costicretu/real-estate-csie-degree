49580cf19673b2225aa33c0966afbc1bd63dc30c
Costi Cretu <costi.cretu25@gmail.com>
2023-04-05 18:28:41 +0300

    delete + edit functionality to the my listing section added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { getAuth, updateProfile } from 'firebase/auth'
 import { useNavigate } from 'react-router'
 import { toast } from 'react-toastify'
-import { updateDoc, doc, collection, query, where, orderBy, getDocs } from 'firebase/firestore'
+import { updateDoc, doc, collection, query, where, orderBy, getDocs, deleteDoc } from 'firebase/firestore'
 import { db } from '../firebase'
 import { FcHome } from 'react-icons/fc'
 import { Link } from 'react-router-dom'
@@ -64,6 +64,19 @@ export default function Profile() {
     }
     fetchUserListings()
   }, [auth.currentUser.uid])
+  async function onDelete(listingID) {
+    if (window.confirm('Are you sure you want to delete?')) {
+      await deleteDoc(doc(db, 'listings', listingID))
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingID
+      )
+      setListings(updatedListings)
+      toast.success('Successfully deleted the listing')
+    }
+  }
+  function onEdit(listingID) {
+    navigate(`/edit-listing/${listingID}`)
+  }
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -100,7 +113,12 @@ export default function Profile() {
             <h2 className='text-2xl text-center font-semibold mb-6'>My listings</h2>
             <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 mt-6 mb-6'>
               {listings.map((listing) => (
-                <ListingItem key={listing.id} id={listing.id} listing={listing.data}/>
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)} />
               ))}
             </ul>
           </>

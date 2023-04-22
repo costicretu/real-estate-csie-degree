83581a96000fb64cc58e4341de3e82f4984b1b5d
Costi Cretu <costi.cretu25@gmail.com>
2023-04-22 15:57:24 +0300

    styling the offers and announces pages

--- src/pages/Announces.jsx
+++ src/pages/Announces.jsx
@@ -5,6 +5,7 @@ import { db } from '../firebase'
 import Spinner from '../components/Spinner'
 import ListingItem from '../components/ListingItem'
 import { AiOutlineSearch } from 'react-icons/ai'
+import { BsFillBuildingFill } from 'react-icons/bs'
 
 export default function Announces() {
   const [listings, setListings] = useState(null)
@@ -130,71 +131,80 @@ export default function Announces() {
     fetchListings();
   }
   return (
-    <div className='justify-center items-center'>
-      <h1 className="text-3xl text-center mt-6 font-bold mb-5">Anunțuri</h1>
-      <div className="flex flex-wrap mx-auto my-auto ">
-        <div className=" flex flex-col mt-2 ml-7 md:w-[67%] lg:w-[15%] lg:ml-30" id="pentruMine">
-          <div className="flex items-center justify-center">
-            <label className="mr-4">
-              <input
-                type="radio"
-                name="type"
-                value="rent"
-                checked={type === "rent"}
-                onChange={handleTypeChange}
-                className="mr-2"
-              />
-              De închiriat
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="type"
-                value="sale"
-                checked={type === "sale"}
-                onChange={handleTypeChange}
-                className="mr-2"
-              />
-              De vânzare
-            </label>
+    <div>
+      <h1 className="text-3xl text-center mt-6 font-bold mb-6">Anunțuri</h1>
+      <div className='max-w-6xl mx-2 px-3'>
+        <div className="flex flex-col md:flex-row">
+          <div className="w-full md:w-[30%] lg:w-[30%] mb-4 mr-3 md:mb-0 bg-slate-500 rounded px-2 py-2 shadow-lg" id="pentruMine">
+            <div className=' bg-red-500 text-white rounded-lg flex items-center px-2 mt-1  my-4  after:border-t after:flex-1 after:border-gray-300'>
+              <AiOutlineSearch className='px-1 text-4xl' />
+            </div>
+            <div className="flex items-center justify-center px-1 py-1 mt-6 text-gray-100">
+              <label className='mr-3 text-4xl'>
+                <BsFillBuildingFill className='bg-red-500 rounded px-1' />
+              </label>
+              <label className="mr-3 text-md font-medium">
+                <input
+                  type="radio"
+                  name="type"
+                  value="rent"
+                  checked={type === "rent"}
+                  onChange={handleTypeChange}
+                  className="mr-1 transition ease-in-out"
+                />
+                Închiriere
+              </label>
+              <label className='text-md font-medium '>
+                <input
+                  type="radio"
+                  name="type"
+                  value="sale"
+                  checked={type === "sale"}
+                  onChange={handleTypeChange}
+                  className="mr-1 transition ease-in-out"
+                />
+                Vânzare
+              </label>
+            </div>
+            <select name="property" value={property} onChange={handlePropertyChange} className="text-md font-medium transition ease-in-out block w-full mt-6 px-4 py-2 rounded-md bg-gray-100 border-gray-300 focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500">
+              <option value="apartment">Apartament</option>
+              <option value="house">Casă</option>
+              <option value="land">Teren</option>
+            </select>
+            <input type="text" name="address" placeholder="Introdu o adresă..." value={address} onChange={handleAddressChange} className="text-md transition ease-in-out font-medium block w-full mt-6 px-4 py-2 rounded-md bg-gray-100 border-gray-300 focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500" />
+            <div className="flex justify-center mt-8">
+              <button className="bg-red-500 hover:bg-red-600  text-slate-200 px-10 py-1 rounded  text-md font-medium " onClick={handleSearchClick}>
+                Caută acum
+              </button>
+            </div>
           </div>
-          <select name="property" value={property} onChange={handlePropertyChange} className="block w-full mt-4 px-4 py-2 rounded-md bg-gray-100 border-gray-300 focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500">
-            <option value="apartment">Apartament</option>
-            <option value="house">Casă</option>
-            <option value="land">Teren</option>
-          </select>
-          <input type="text" name="address" placeholder="Introdu o adresă..." value={address} onChange={handleAddressChange} className="block w-full mt-4 px-4 py-2 rounded-md bg-gray-100 border-gray-300 focus:outline-none focus:ring focus:ring-blue-500 focus:border-blue-500" />
-          <button className="relative mt-4 bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded" onClick={handleSearchClick}>
-            Caută
-            <AiOutlineSearch className='absolute right-3 top-1/2 transform -translate-y-1/2 text-xl cursor-pointer' />
-          </button>
-        </div>
-        <div className="max-w-6xl  px-3" id="pentruAnunturi">
-          {loading ? (
-            <Spinner />
-          ) : listings && listings.length > 0 ? (
-            <>
-              <main>
-                <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5s mb-6">
-                  {listings.map((listing) => (
-                    <ListingItem key={listing.id} id={listing.id} listing={listing.data} />
-                  ))}
-                </ul>
-              </main>
-              {lastFetchListing && (
-                <div className="flex justify-center items-center">
-                  <button className=" bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded" onClick={() => onFetchMoreListings(type, property, address)}>
-                    Afișează mai multe
-                  </button>
+          <div className="w-full md:flex-2 md:w-[100%] lg:w-[100%]" id="pentruAnunturi">
+            {loading ? (
+              <Spinner />
+            ) : listings && listings.length > 0 ? (
+              <>
+                <main>
+                  <ul className="sm:grid sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3">
+                    {listings.map((listing) => (
+                      <ListingItem key={listing.id} id={listing.id} listing={listing.data} />
+                    ))}
+                  </ul>
+                </main>
 
-                </div>
-              )}
-            </>
-          ) : (
-            <p>Nu există anunțuri recente momentan</p>
-          )}
+              </>
+            ) : (
+              <p className='text-2xl font-normal'>Nu există anunțuri momentan</p>
+            )}
+          </div>
         </div>
       </div>
+      {lastFetchListing && (
+        <div className="flex justify-center items-center">
+          <button className=" bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded" onClick={() => onFetchMoreListings(type, property, address)}>
+            Afișează mai multe
+          </button>
+        </div>
+      )}
     </div>
   )
 }

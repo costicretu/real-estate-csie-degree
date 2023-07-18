cf91b3801de9201f9350c4d164931dcafee71591
costicretu <115085746+costicretu@users.noreply.github.com>
2023-07-18 17:10:18 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB6C13kXzhZOzkk-sdHb0Wcm2QLwkVT3ak",
-    authDomain: "real-estate-csie-degree.firebaseapp.com",
-    projectId: "real-estate-csie-degree",
-    storageBucket: "real-estate-csie-degree.appspot.com",
-    messagingSenderId: "1085883723439",
-    appId: "1:1085883723439:web:374fbf47464af33a3fee5a"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 initializeApp(firebaseConfig);
